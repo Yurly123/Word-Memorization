1a69dcee10dde3dbc5726f178e3617d71f7a97bb
Yurly123 <joo7won@gmail.com>
2022-09-17 19:24:19 +0900

    Added Word Save System
    Now you can save and load words with xml.
    Save and load occurs at the same time and automatically at start and when you clicked randomize button.

--- Assets/Scripts/WordUpdater.cs
+++ Assets/Scripts/WordUpdater.cs
@@ -3,33 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Xml;
+using System.IO;
+using System.Linq;
 
 public class WordUpdater : MonoBehaviour
 {
-    public TextMeshProUGUI Kanji;
-    public TextMeshProUGUI Furigana;
-    public TextMeshProUGUI Meaning;
+    public TextMeshProUGUI KanjiText;
+    public TextMeshProUGUI FuriganaText;
+    public TextMeshProUGUI MeaningText;
     
     List<Word> WordList = new List<Word>();
     int index = 0;
     // Start is called before the first frame update
     void Awake() {
-        WordList.Add(new Word("急ぐ","いそぐ","서두르다"));
-        WordList.Add(new Word("糸","いと","실"));
-        WordList.Add(new Word("都会","とかい","도시"));
-        WordList.Add(new Word("都合","つごう","사정, 형편"));
+        WordList.Add(new Word("青い", "あおい", "파랗다"));
+        WordList.Add(new Word("味", "あじ", "맛"));
+        RandomizeWordList();
     }
 
-    void UpdateWord()
+    void UpdateWord(int index)
     {
         Word word = WordList[index];
-        Kanji.text = word.Kanji;
-        Furigana.text = word.Furigana;
-        Meaning.text = word.Meaning;
+        KanjiText.text = word.Kanji;
+        FuriganaText.text = word.Furigana;
+        MeaningText.text = word.Meaning;
     }
 
     private void Start() {
-        UpdateWord();
+        UpdateWord(0);
     }
 
     // Update is called once per frame
@@ -42,8 +44,7 @@ public class WordUpdater : MonoBehaviour
     {
         if(index > 0)
         {
-            --index;
-            UpdateWord();
+            UpdateWord(--index);
         }
     }
 
@@ -51,9 +52,91 @@ public class WordUpdater : MonoBehaviour
     {
         if(index < WordList.Count - 1)
         {
-            ++index;
-            UpdateWord();
+            UpdateWord(++index);
         }
+    }
+
+    public void RandomizeWordList()
+    {
+        LoadWordList();
+        for (int i = 0; i < WordList.Count; ++i)
+        {
+            int RandIndex = Random.Range(0, WordList.Count);
+            Word temp = WordList[i];
+            WordList[i] = WordList[RandIndex];
+            WordList[RandIndex] = temp;
+        }
+        index = 0;
+        UpdateWord(index);
+    }
+
+    void LoadWordList()
+    {
+        XmlDocument Document = new XmlDocument();
+        List<Word> XmlWordList = new List<Word>();
+        try
+        {
+            Document.Load(Application.persistentDataPath + @"\Words.xml");
+            XmlElement Words = Document["Words"];
+
+            foreach (XmlElement Word in Words.ChildNodes)
+            {
+                Word word = new Word();
+                word.Kanji = Word.GetAttribute("한자");
+                word.Furigana = Word.GetAttribute("후리가나");
+                word.Meaning = Word.GetAttribute("의미");
+                XmlWordList.Add(word);
+            }
+        }
+        catch { }
+
+        try
+        {
+            Document.Load(Application.dataPath + @"\Words.xml");
+            XmlElement Words = Document["Words"];
+
+            foreach (XmlElement Word in Words.ChildNodes)
+            {
+                Word word = new Word();
+                word.Kanji = Word.GetAttribute("한자");
+                word.Furigana = Word.GetAttribute("후리가나");
+                word.Meaning = Word.GetAttribute("의미");
+                XmlWordList.Add(word);
+            }
+        }
+        catch { }
+
+        foreach (Word XmlWord in XmlWordList)
+        {
+            var OverlappingWords = from Word in WordList
+                                   where Word.Kanji == XmlWord.Kanji
+                                   where Word.Furigana == XmlWord.Furigana
+                                   where Word.Meaning == Word.Meaning
+                                   select Word;
+            if (OverlappingWords.Count() == 0)
+            {
+                WordList.Add(XmlWord);
+            }
+        }
+        SaveWordList();
+    }
+    void SaveWordList()
+    {
+        XmlDocument Document = new XmlDocument();
+        XmlElement Words = Document.CreateElement("Words");
+        Document.AppendChild(Words);
+ 
+        foreach (var Word in WordList)
+        {
+            XmlElement WordElement = Document.CreateElement("Word");
+            WordElement.SetAttribute("한자", Word.Kanji);
+            WordElement.SetAttribute("후리가나", Word.Furigana);
+            WordElement.SetAttribute("의미", Word.Meaning);
+            Words.AppendChild(WordElement);
+        }
+
+        Document.Save(Application.persistentDataPath + @"\Words.xml");
+        Document.Save(Application.dataPath + @"\Words.xml");
     }
 }
 
@@ -68,4 +151,5 @@ class Word
         this.Furigana = Furigana;
         this.Meaning = Meaning;
     }
+    public Word() : this(string.Empty, string.Empty, string.Empty) { }
 }
